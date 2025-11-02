5e3c142173401113bfb03bd3072b4eb6e934d662
Alparslan <alp84202@gmail.com>
2025-11-02 15:25:18 +0300

    güncelleme

--- veri.js
+++ veri.js
@@ -106,6 +106,92 @@ export const kategoriVerisi = {
       ]
     },
 
+{ 
+      isim: "Margarita Pizza", 
+      resim: "./img/pizzalar/pizzaMargarita.jpg", 
+      fiyat: "250₺", 
+      malzemeler: ["Domates", "Mozzarella", "Fesleğen"], 
+      extras: [
+        { isim: "Ekstra Patates", fiyat: "15₺", resim: "./img/patates.jpeg" },
+        { isim: "Soğan Halkası", fiyat: "12₺", resim: "./img/sogan.jpeg" },
+        { isim: "Çıtır Peynir", fiyat: "10₺", resim: "./img/citirPeynir.jpeg" },
+        { isim: "Ekstra Peynir", fiyat: "10₺", resim: "./img/peynir.jpg" },
+        { isim: "Ekstra Salata", fiyat: "10₺", resim: "./img/salata.jpeg" }
+      ]
+    },
+
+
+    { 
+      isim: "Pepperoni Pizza", 
+      resim: "./img/pizzalar/pizzaPepperoni.jpg", 
+      fiyat: "370₺", 
+      malzemeler: ["Pepperoni", "Mozzarella", "Domates"], 
+      extras: [
+        { isim: "Ekstra Patates", fiyat: "15₺", resim: "./img/patates.jpeg" },
+        { isim: "Soğan Halkası", fiyat: "12₺", resim: "./img/sogan.jpeg" },
+        { isim: "Çıtır Peynir", fiyat: "10₺", resim: "./img/citirPeynir.jpeg" },
+        { isim: "Ekstra Peynir", fiyat: "10₺", resim: "./img/peynir.jpg" },
+        { isim: "Ekstra Salata", fiyat: "10₺", resim: "./img/salata.jpeg" }
+      ]
+    },
+
+
+    { 
+      isim: "Karışık Pizza", 
+      resim: "./img/pizzalar/pizzaKarisik.jpg", 
+      fiyat: "300₺", 
+      malzemeler: ["Pepperoni", "Mozzarella", "Domates", "Mozzarella"], 
+      extras: [
+        { isim: "Ekstra Patates", fiyat: "15₺", resim: "./img/patates.jpeg" },
+        { isim: "Soğan Halkası", fiyat: "12₺", resim: "./img/sogan.jpeg" },
+        { isim: "Çıtır Peynir", fiyat: "10₺", resim: "./img/citirPeynir.jpeg" },
+        { isim: "Ekstra Peynir", fiyat: "10₺", resim: "./img/peynir.jpg" },
+        { isim: "Ekstra Salata", fiyat: "10₺", resim: "./img/salata.jpeg" }
+      ]
+    },
+
+    { 
+      isim: "Jambonlu Pizza", 
+      resim: "./img/pizzalar/pizzaJambon.jpg", 
+      fiyat: "510₺", 
+      malzemeler: ["Pepperoni", "Mozzarella", "Domates"], 
+      extras: [
+        { isim: "Ekstra Patates", fiyat: "15₺", resim: "./img/patates.jpeg" },
+        { isim: "Soğan Halkası", fiyat: "12₺", resim: "./img/sogan.jpeg" },
+        { isim: "Çıtır Peynir", fiyat: "10₺", resim: "./img/citirPeynir.jpeg" },
+        { isim: "Ekstra Peynir", fiyat: "10₺", resim: "./img/peynir.jpg" },
+        { isim: "Ekstra Salata", fiyat: "10₺", resim: "./img/salata.jpeg" }
+      ]
+    },
+
+    { 
+      isim: "Muriana Pizza", 
+      resim: "./img/pizzalar/pizzaMuriana.jpg", 
+      fiyat: "450₺", 
+      malzemeler: ["Pepperoni", "Mozzarella", "Domates"], 
+      extras: [
+        { isim: "Ekstra Patates", fiyat: "15₺", resim: "./img/patates.jpeg" },
+        { isim: "Soğan Halkası", fiyat: "12₺", resim: "./img/sogan.jpeg" },
+        { isim: "Çıtır Peynir", fiyat: "10₺", resim: "./img/citirPeynir.jpeg" },
+        { isim: "Ekstra Peynir", fiyat: "10₺", resim: "./img/peynir.jpg" },
+        { isim: "Ekstra Salata", fiyat: "10₺", resim: "./img/salata.jpeg" }
+      ]
+    },
+
+    { 
+      isim: "Puglia Pizza", 
+      resim: "./img/pizzalar/pizzaPuglia.jpg", 
+      fiyat: "375₺", 
+      malzemeler: ["Pepperoni", "Mozzarella", "Domates"], 
+      extras: [
+        { isim: "Ekstra Patates", fiyat: "15₺", resim: "./img/patates.jpeg" },
+        { isim: "Soğan Halkası", fiyat: "12₺", resim: "./img/sogan.jpeg" },
+        { isim: "Çıtır Peynir", fiyat: "10₺", resim: "./img/citirPeynir.jpeg" },
+        { isim: "Ekstra Peynir", fiyat: "10₺", resim: "./img/peynir.jpg" },
+        { isim: "Ekstra Salata", fiyat: "10₺", resim: "./img/salata.jpeg" }
+      ]
+    },
+
 
 
   ],
